f6fc14648ef3063424c5953539010aa91157192f
Krisztian Korosi <koeroesi86@gmail.com>
2023-03-06 08:01:14 +0100

    Debug GH ref type

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 
-const { GITHUB_RUN_ID, GITHUB_REF_NAME } = process.env;
+const { GITHUB_RUN_ID, GITHUB_REF_NAME, GITHUB_REF_TYPE } = process.env;
 
 if (!GITHUB_REF_NAME) {
   throw new Error(`GITHUB_REF_NAME: ${GITHUB_REF_NAME}`);
@@ -9,6 +9,9 @@ if (!GITHUB_REF_NAME) {
 if (!GITHUB_RUN_ID) {
   throw new Error(`GITHUB_RUN_ID: ${GITHUB_RUN_ID}`);
 }
+
+console.log('GITHUB_REF_TYPE', GITHUB_REF_TYPE);
+console.log('GITHUB_REF_NAME', GITHUB_REF_NAME);
 
 const now = new Date();
 const version = `${`${now.getFullYear()}`.substring(2)}.${`${now.getMonth() + 1}`.padStart(2, '0')}.${GITHUB_RUN_ID}-${GITHUB_REF_NAME}`;
